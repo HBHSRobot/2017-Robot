c886fb32380aca77e5406be98d513b7312f8c8c4
Kevin Traw <ktraw2@gmail.com>
2017-02-15 15:54:55 -0800

    Trying to desperately fix eclipse's problems

--- src/org/usfirst/frc/team5966/robot/Robot.java
+++ src/org/usfirst/frc/team5966/robot/Robot.java
@@ -1,4 +1,3 @@
-
 package org.usfirst.frc.team5966.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -102,7 +101,7 @@ public class Robot extends IterativeRobot {
 		{ case "My Auto": autonomousCommand
 		 * = new MyAutoCommand(); break; case "Default Auto": default:
 		 * autonomousCommand = new ExampleCommand(); break; }
-		 */
+		 */ 
 
 		// schedule the autonomous command (example)
 		if (autonomousCommand != null)
@@ -143,112 +142,6 @@ public class Robot extends IterativeRobot {
 			{
 				robotDrives[i].arcadeDrive(driveStick);
 			}
-			Timer.delay(0.01);
-		}
-	}
-
-	/**
-	 * This function is called periodically during test mode
-	 */
-	@Override
-	public void testPeriodic() {
-		LiveWindow.run();
-	}
-}
-
-		//right motors are PWM 3, 4, 5
-		for (int i = 0; i < NUM_MOTORS; i++)
-		{
-			leftMotors[i] = new VictorSP(i);
-			rightMotors[i] = new VictorSP(i + NUM_MOTORS);
-			robotDrives[i] = new RobotDrive(leftMotors[i],
-					 rightMotors[i]);
-		}
-		leftMotors[0].set(0.7);
-		//XBOX Controller
-		driveStick = new Joystick(0);
-		
-	}
-	
-
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-		
-	 */
-	@Override
-	public void disabledInit() {
-
-	}
-
-	@Override
-	public void disabledPeriodic() {
-		Scheduler.getInstance().run();
-	}
-
-	/**
-		
-		
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
-	 *
-	 * You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
-	 */
-	@Override
-	public void autonomousInit() {
-		autonomousCommand = chooser.getSelected();
-
-		/*
-		 * String autoSelected = SmartDashboard.getString("Auto Selector",
-		 * "Default"); switch(autoSelected) 
-		{ case "My Auto": autonomousCommand
-		 * = new MyAutoCommand(); break; case "Default Auto": default:
-		 * autonomousCommand = new ExampleCommand(); break; }
-		 */
-
-		// schedule the autonomous command (example)
-		if (autonomousCommand != null)
-			autonomousCommand.start();
-	}
-
-	/**
-	 * This function is called periodically during autonomous
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		Scheduler.getInstance().run();
-	}
-
-	@Override
-	
-	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-		if (autonomousCommand != null)
-			autonomousCommand.cancel();
-	}
-
-	/**
-	 * This function is called periodically during operator control
-	 */
-	@Override
-	public void teleopPeriodic() {
-		Scheduler.getInstance().run();
-		while (isOperatorControl() && isEnabled())
-		{
-			//drive motor sets 1, 2, and 3
-			//for (int i = 0; i < NUM_MOTORS; i++)
-			//{
-				robotDrives[0].arcadeDrive(driveStick);
-			//}
 			Timer.delay(0.01);
 		}
 	}
